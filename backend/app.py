25f0abbaafabb271a8e8c59873d8a52a20d8e7ed
Davina Nesigamye Muyinza <davinamuyinza@gmail.com>
2026-05-07 21:41:06 +0200

    Add first OpenAI job analysis call

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 app = FastAPI(title="AI Job Application Assistant")
 
@@ -7,12 +15,28 @@ class JobRequest(BaseModel):
     job_description: str
     user_profile: str
 
+def request_ai (job_description: str, user_profile: str):
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=f"""
+        you are a job application assisatnt. 
+        you extract everything i need to know about the job.
+        return the details as a summary of the job description
+
+        job:
+        Description:{job_description} 
+        Profile:{user_profile}
+        """
+    )
+
+    return response.output_text
+
 @app.post("/analyse-job")
 def analyse_job(payload: JobRequest):
+    ai_result= request_ai(payload.job_description, payload.user_profile)
     return {
-        "received": True,
-        "job_description": payload.job_description,
-        "user_profile": payload.user_profile
+        "job_summary": ai_result
+        
     }
 
 
